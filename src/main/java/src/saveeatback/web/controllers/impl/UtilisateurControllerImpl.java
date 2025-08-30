dcd76f6c028e3ae834c4daac15498af859f9465f
babacar <babacarmbaye2400@gmail.com>
2025-08-30 13:28:21 +0000

    correction mapper utilisateur

--- src/main/java/src/saveeatback/web/controllers/impl/UtilisateurControllerImpl.java
+++ src/main/java/src/saveeatback/web/controllers/impl/UtilisateurControllerImpl.java
@@ -45,7 +45,7 @@ public class UtilisateurControllerImpl implements UtilisateurController {
             return new ResponseEntity<>(restResponse,HttpStatus.BAD_REQUEST);
         }
 
-        Utilisateur user = this.userMapper.signupDtoToUser(userRequest, this.passwordEncoder);
+        Utilisateur user = this.userMapper.signupDtoToUser(userRequest);
         var userPosted = this.userService.create(user);
         if(userPosted !=null){
             UserCreateResponse userResponse = this.userMapper.toUserCreateResponse(userPosted);
